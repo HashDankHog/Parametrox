a5b88476a77aee907b1869d319339dba6ad1846a
HashDankHog <madelinecooreman@gmail.com>
2026-05-27 18:10:28 -0600

    Refactor to remove need for generics

--- solver/src/parameter.rs
+++ solver/src/parameter.rs
@@ -1,12 +1,12 @@
-pub enum Value<T: Fn() -> f64> {
+pub enum Value {
     Constant(f64),
-    Expression(T),
+    Expression(Box<dyn Fn() -> f64>),
 }
-impl<T: Fn() -> f64> Value<T> {
+impl Value {
     pub fn value(&self) -> f64 {
         match self {
             Value::Constant(constant) => *constant,
-            Value::Expression(expression) => expression(),
+            Value::Expression(expression) => (*expression)(),
         }
     }
 }
@@ -19,7 +19,7 @@ mod tests {
 
     #[test]
     fn value_test_constant() {
-        let a = Value::<fn() -> f64>::Constant(0.0);
+        let a = Value::Constant(0.0);
         let a_value = a.value();
 
         let expected_value = 0.0;
@@ -28,16 +28,17 @@ mod tests {
 
     #[test]
     fn value_test_expression() {
-        let value = Rc::new(RefCell::new(5.0));
-        let a = Value::Expression(|| *value.borrow());
+        let value = Rc::new(RefCell::new(1.0));
+        let closure_value = value.clone();
+        let a = Value::Expression(Box::new(move || 2.0 * (*closure_value.borrow()) + 1.0));  //2x+1
         let a_value1 = a.value();
 
-        let expected_value1 = 5.0;
+        let expected_value1 = 3.0;
 
         *value.borrow_mut() += 1.0;
 
         let a_value2 = a.value();
-        let expected_value2: f64 = 6.0;
+        let expected_value2: f64 = 5.0;
         assert_eq!(expected_value1, a_value1);
         assert_eq!(expected_value2, a_value2);
     }
