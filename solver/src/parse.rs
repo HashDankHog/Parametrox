0ea4ca2f9585e621c606e86395f771da9943093e
HashDankHog <madelinecooreman@gmail.com>
2026-05-29 15:59:32 -0600

    Add super basic shunting yard parser

--- solver/src/parse.rs
+++ solver/src/parse.rs
@@ -1,15 +1,20 @@
-pub fn tokenize(expression: &String, ) -> Vec<String> {
+use std::collections::HashMap;
+
+pub fn tokenize(expression_raw: &String) -> Vec<String> {
+    let mut expression = expression_raw.clone(); // clone might make this more inneficient than my original solution 
+
+    expression.make_ascii_lowercase();
+    expression = expression.replace(" ", "");
     //tokinizes the input expression
     let mut tokens: Vec<String> = Vec::new();
     let mut token = String::new();
     // I should probably create an enum for this instead of using magic numbers but there might be a performance hit
     let mut token_type = 0;
 
-  
     /* tokenizing rules
     each token can be either an operation
     a parantheses
-    the letter p followed by an integer 
+    the letter p followed by an integer
     or an integer
     */
     /* token_type
@@ -20,81 +25,105 @@ pub fn tokenize(expression: &String, ) -> Vec<String> {
     4: prefix op
     5: postfix op
      */
-    
+
     for character in expression.chars() {
         match token_type {
-            0 => { // beginning of expression
-                if character != ' ' {
-                    token.push(character);
-                }
+            0 => {
+                // beginning of expression
+
+                token.push(character);
                 match character {
-                    ' '       => token_type = 0, 
                     '0'..='9' => token_type = 1,
-                    'p' | 'P' => token_type = 2,
-                    '('       => token_type = 4,
-                     _        => panic!("invalid expression"),
+                    'p' => token_type = 2,
+                    '(' => token_type = 4,
+                    _ => panic!("invalid expression"),
                 }
-            },
-            1 => { // constant
+            }
+            1 => {
+                // constant
                 match character {
-                    ' '             => token_type = 1,
                     '0'..='9' | '.' => token.push(character),
-                    'p' | 'P'       => {
+                    'p' => {
                         tokens.push(token);
                         tokens.push(String::from("*"));
 
                         token = String::from("p");
                         token_type = 2;
-                    },
-                    '+' | '-' | '/' | '*' => { // should be operations[i] but okay compilar pop off girly pop give me that error you go girl
+                    }
+                    '+' | '-' | '/' | '*' => {
                         tokens.push(token);
                         token = String::from(character);
                         token_type = 3;
-                    },
-                    _                => panic!("invalid expression"), // son im crine why is this unreachable. girly pop you lied
-                }
-            },
-            2 => {
-                match character {
-                    ' '       => token_type = 2,
-                    '0'..='9' => token.push(character),
-                    'p' | 'P' => {
-                        tokens.push(token);
-                        tokens.push(String::from("*"));
-                        token = String::from("p");
-                    },
-                    '+' | '-' | '/' | '*' => { // should be operations[i] but okay compilar pop off girly pop give me that error you go girl
-                        tokens.push(token);
-                        token = String::from(character);
-                        token_type = 3;
-                    },
-                    _ => panic!("invalid expression"), // son im crine why is this unreachable. girly pop you lied
+                    }
+                    _ => panic!("invalid expression"),
                 }
             }
+            2 => match character {
+                '0'..='9' => token.push(character),
+                'p' => {
+                    tokens.push(token);
+                    tokens.push(String::from("*"));
+                    token = String::from("p");
+                }
+                '+' | '-' | '/' | '*' => {
+                    tokens.push(token);
+                    token = String::from(character);
+                    token_type = 3;
+                }
+                _ => panic!("invalid expression"),
+            },
             3 => {
                 tokens.push(token);
-                // this if else block is fugly and needs to be fixed
-                if character != ' ' {
-                    token = String::from(character);
-                } else {
-                    token = String::new();
-                }
-                match character {
-                    ' '       => token_type = 0,
-                    '0'..='9' => token_type = 1,
-                    'p' | 'P' => token_type = 2,
-                    '('       => token_type = 4,
-                     _        => panic!("invalid expression"),
-                }
-            },
-            _ => unreachable!()
-        }
-        token.make_ascii_lowercase(); //inefficent to make this call every loop 
 
+                token = String::from(character);
+
+                match character {
+                    ' ' => token_type = 0,
+                    '0'..='9' => token_type = 1,
+                    'p' => token_type = 2,
+                    '(' => token_type = 4,
+                    _ => panic!("invalid expression"),
+                }
+            }
+            _ => unreachable!(),
+        }
     }
     tokens.push(token); // pushs last token on stack
     tokens
 }
+
+//shunting yard algorithm
+//autism reference
+pub fn parse(tokens: Vec<String>) -> Vec<String> {
+    let mut precidence = HashMap::new();
+    precidence.insert(String::from("+"), 1);
+    precidence.insert(String::from("-"), 1);
+    precidence.insert(String::from("*"), 2);
+    precidence.insert(String::from("/"), 2);
+    let mut output = Vec::new();
+    let mut operation_stack = Vec::new();
+    operation_stack.push(String::new());
+    for token in tokens {
+        let value = precidence.get(&token).copied().unwrap_or(0);
+        match value {
+            0 => output.push(token),
+            _ => {
+                if value > precidence.get(&operation_stack[0]).copied().unwrap_or(3) {
+                    output.push(token);
+                } else {
+                    operation_stack.push(token);
+                }
+            }
+        }
+    }
+    operation_stack.reverse();
+    for operator in operation_stack {
+        output.push(operator);
+    }
+    output.pop();
+    output
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -118,19 +147,68 @@ mod tests {
     fn lexer_test_operation() {
         let test_expression = String::from("1.5+3.5/2-100");
         let tokens = tokenize(&test_expression);
-        assert_eq!(tokens, vec![String::from("1.5"), String::from("+"), String::from("3.5"), String::from("/"), String::from("2"), String::from("-"), String::from("100")])
+        assert_eq!(
+            tokens,
+            vec![
+                String::from("1.5"),
+                String::from("+"),
+                String::from("3.5"),
+                String::from("/"),
+                String::from("2"),
+                String::from("-"),
+                String::from("100")
+            ]
+        )
     }
 
     #[test]
     fn lexer_test_prefix() {
         let test_expression = String::from("4p");
         let tokens = tokenize(&test_expression);
-        assert_eq!(tokens, vec![String::from("4"),String::from("*"),String::from("p")])
+        assert_eq!(
+            tokens,
+            vec![String::from("4"), String::from("*"), String::from("p")]
+        )
     }
     #[test]
-    fn lexer_test_whitespace(){
+    fn lexer_test_whitespace() {
         let test_expression = String::from(" P 1 + 3 . 0 *   10   ");
         let tokens = tokenize(&test_expression);
-        assert_eq!(tokens, vec![String::from("p1"),String::from("+"), String::from("3.0"), String::from("*"), String::from("10")])
+        assert_eq!(
+            tokens,
+            vec![
+                String::from("p1"),
+                String::from("+"),
+                String::from("3.0"),
+                String::from("*"),
+                String::from("10")
+            ]
+        )
+    }
+
+    #[test]
+    fn parser_test_operation() {
+        let tokens = vec![
+            String::from("1"),
+            String::from("+"),
+            String::from("2"),
+            String::from("-"),
+            String::from("1"),
+            String::from("*"),
+            String::from("3"),
+        ];
+        let parsed_expression = parse(tokens);
+        assert_eq!(
+            parsed_expression,
+            vec![
+                String::from("1"),
+                String::from("2"),
+                String::from("1"),
+                String::from("3"),
+                String::from("*"),
+                String::from("-"),
+                String::from("+")
+            ]
+        )
     }
 }
