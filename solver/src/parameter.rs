cac9886637e96ddfceee110282afb1e4c17dc54c
HashDankHog <madelinecooreman@gmail.com>
2026-06-06 21:14:13 -0500

    Add basic operator overrides for +,-,/,*

--- solver/src/parameter.rs
+++ solver/src/parameter.rs
@@ -1,9 +1,10 @@
-use crate::parse::interpret;
+use crate::parse::{interpret, simplify};
 use std::{cell::RefCell, collections::HashMap};
 use std::rc::Rc;
+use std::ops::{Add, Sub, Mul, Div};
 
 
-
+#[derive(PartialEq, Debug, Clone)]
 pub struct Parameter {
     pub expression: Vec<String>,
     pub value: f64,
@@ -21,11 +22,93 @@ impl Parameter {
                     ('t', Box::new(|lhs: f64, rhs: f64| vec![lhs.to_string(), rhs.tan().to_string()])),
                 ]);
         self.value = interpret(&self.expression, &operators, parameters,1)
-        
     }
+
+    
 }
 impl Default for Parameter {
     fn default() -> Self {
         Parameter { expression: vec![String::from("0")], value: 0.0 }
     }
+}
+impl Add for Parameter {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self { //will probably change to pointers but thats wierd
+        let mut expression = self.expression.clone(); //unnessecary?
+        expression.extend(rhs.expression);
+        expression.push(String::from("+"));
+        Self {
+            expression: expression,
+            value: self.value + rhs.value,
+        }
+    }
+}
+impl Sub for Parameter {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self { //will probably change to pointers but thats wierd
+        let mut expression = self.expression.clone(); //unnessecary?
+        expression.extend(rhs.expression);
+        expression.push(String::from("-"));
+        Self {
+            expression: expression,
+            value: self.value - rhs.value,
+        }
+    }
+}
+impl Mul for Parameter {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self { //will probably change to pointers but thats wierd
+        let mut expression = self.expression.clone(); //unnessecary?
+        expression.extend(rhs.expression);
+        expression.push(String::from("*"));
+        Self {
+            expression: expression,
+            value: self.value * rhs.value,
+        }
+    }
+}
+impl Div for Parameter {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self { //will probably change to pointers but thats wierd
+        let mut expression = self.expression.clone(); //unnessecary?
+        expression.extend(rhs.expression);
+        expression.push(String::from("/"));
+        Self {
+            expression: expression,
+            value: self.value / rhs.value,
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parameter_add_test() {
+        let parameter_1 = Parameter{expression: vec![String::from("1")], value: 1.0 };
+        let parameter_2 = Parameter{expression: vec![String::from("2")], value: 2.0 };
+        assert_eq!(parameter_1 + parameter_2, Parameter{expression: vec![String::from("1"), String::from("2"), String::from("+")], value: 3.0 })
+    }
+
+    #[test]
+    fn parameter_sub_test() {
+        let parameter_1 = Parameter{expression: vec![String::from("1")], value: 1.0 };
+        let parameter_2 = Parameter{expression: vec![String::from("2")], value: 2.0 };
+        assert_eq!(parameter_1 - parameter_2, Parameter{expression: vec![String::from("1"), String::from("2"), String::from("-")], value: -1.0 })
+    }
+
+    #[test]
+    fn parameter_mul_test() {
+        let parameter_1 = Parameter{expression: vec![String::from("1")], value: 1.0 };
+        let parameter_2 = Parameter{expression: vec![String::from("2")], value: 2.0 };
+        assert_eq!(parameter_1 * parameter_2, Parameter{expression: vec![String::from("1"), String::from("2"), String::from("*")], value: 2.0 })
+    }
+
+    #[test]
+    fn parameter_div_test() {
+        let parameter_1 = Parameter{expression: vec![String::from("1")], value: 1.0 };
+        let parameter_2 = Parameter{expression: vec![String::from("2")], value: 2.0 };
+        assert_eq!(parameter_1 / parameter_2, Parameter{expression: vec![String::from("1"), String::from("2"), String::from("/")], value: 0.5 })
+    }
+
 }
