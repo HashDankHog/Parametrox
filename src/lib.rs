4efa2e0ba6196aa78271f7b325c66961cedb62ce
HashDankHog <madelinecooreman@gmail.com>
2026-06-19 16:24:09 -0600

    Optimized, cutting render time down from 4.3 seconds to 90ms

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,34 @@
+//this file implements constructive solid geometry trees
+//  methods for converting b-rep to csg and vice versa can be found in /src/bin/CAD
+//  
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use scene::render::{Image, Color};
 use solver;
 use std::sync::{LazyLock, Mutex};
 
-static SCREEN: LazyLock<Mutex<Image>> = LazyLock::new(|| Mutex::new(Image::default()));
-static EMPTY_SCREEN: LazyLock<Image> = LazyLock::new(|| Image::from((1000, 1000)));
-
+static SCREEN: LazyLock<Mutex<Vec<u8>>> = LazyLock::new(|| Mutex::new(vec![0]));
+static EMPTY_SCREEN: LazyLock<Vec<u8>> = LazyLock::new(|| {
+    let mut a: Vec<u8> = vec![];
+    for _ in 0..1000000 {
+        a.push(0);
+        a.push(0);
+        a.push(0);
+        a.push(255);
+    }
+    a
+});
+static HEIGHT: LazyLock<Mutex<usize>> = LazyLock::new(|| Mutex::new(0));
+static WIDTH: LazyLock<Mutex<usize>> = LazyLock::new(|| Mutex::new(0));
 #[tauri::command]
 fn update_canvas() -> Vec<u8> {
-    (*SCREEN.lock().unwrap()).to_canvas()
+    (*SCREEN.lock().unwrap()).clone()
 }
 #[tauri::command]
 fn create_canvas(width: usize, height: usize) {
-    *SCREEN.lock().unwrap() = Image::from((width, height));
+    *SCREEN.lock().unwrap() = vec![0; width*height*4];
+    *WIDTH.lock().unwrap() = width;
+    *HEIGHT.lock().unwrap() = height;
 }
 
 #[tauri::command]
@@ -23,12 +38,36 @@ fn clear_canvas() {
 
 #[tauri::command]
 fn set_pixel(row: usize, colum: usize, c: [u8; 3]){
-    let color = Color(c[0], c[1], c[2]);
-    (*SCREEN.lock().unwrap()).canvas[row][colum] = color;
+    
+    let mut temp = SCREEN.lock().unwrap();
+    let width = *WIDTH.lock().unwrap();
+    (*temp)[4*(width*row+colum)] = c[0];
+    (*temp)[4*(width*row+colum)+1] = c[1];
+    (*temp)[4*(width*row+colum)+2] = c[2];
+    (*temp)[4*(width*row+colum)+3] = 255;
 }
 pub fn run() {
     tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![update_canvas, create_canvas, clear_canvas, set_pixel])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+pub fn bench() {
+    use std::time::Instant;
+    let now = Instant::now();
+
+    create_canvas(1000, 1000);
+    update_canvas();
+    for i in 100..200 {
+        clear_canvas();
+        for row in 0..100 {
+            for colum in 0..100{
+                set_pixel(i+row, i+colum, [255,255,255]);
+            }
+        }
+        update_canvas();
+    }
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }
